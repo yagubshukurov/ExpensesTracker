ff69a79f3737a177fd0d3f944e0590dc9f3261b6
Yagub Shukurov <shukurov23@outlook.com>
2020-10-31 03:40:58 +0400

    Initial commit

---
+++ src/test/java/com/tracker/expensestrackerapi/ExpensesTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.expensestrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
