cec4a068c024bc10ac0d0c87207985096528f4a6
Yagub Shukurov <shukurov23@outlook.com>
2020-11-01 16:05:21 +0400

    Complete User Registration

--- src/main/java/com/tracker/expensestrackerapi/repositories/UserRepositoryImpl.java
+++ src/main/java/com/tracker/expensestrackerapi/repositories/UserRepositoryImpl.java
@@ -4,19 +4,40 @@ import com.tracker.expensestrackerapi.domain.User;
 import com.tracker.expensestrackerapi.exceptions.EtAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
 public class UserRepositoryImpl implements UserRepository{
 
     private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL," +
-            "PASSWORD) VALUES(NEXT_VAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
+            " PASSWORD) VALUES(NEXTVAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
 
+    private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
+
+    private static final String SQL_FIND_BY_ID = "SELECT USER_ID , FIRST_NAME, LAST_NAME, EMAIL, " +
+            "PASSWORD FROM ET_USERS WHERE USER_ID = ?";
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
         try{
-
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, firstName);
+                ps.setString(2, lastName);
+                ps.setString(3, email);
+                ps.setString(4, password);
+                return ps;
+            }, keyHolder);
+            return (Integer) keyHolder.getKeys().get("USER_ID");
         } catch (Exception e) {
             throw new EtAuthException("Invalid details. Failed to create account");
         }
@@ -29,11 +50,20 @@ public class UserRepositoryImpl implements UserRepository{
 
     @Override
     public Integer getCountByEmail(String email) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_COUNT_BY_EMAIL, new Object[]{email}, Integer.class);
     }
 
     @Override
     public User findById(Integer userID) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, new Object[]{userID}, userRowMapper);
     }
+
+    private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
+       return new User(
+               rs.getInt("USER_ID"),
+               rs.getString("FIRST_NAME"),
+               rs.getString("LAST_NAME"),
+               rs.getString("EMAIL"),
+               rs.getString("PASSWORD"));
+    });
 }
