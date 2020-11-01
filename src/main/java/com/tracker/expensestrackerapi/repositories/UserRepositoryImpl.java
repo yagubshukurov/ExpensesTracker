4465c4d6e97ba04076794a2852dc7732b44e14b5
Yagub Shukurov <shukurov23@outlook.com>
2020-11-01 21:19:19 +0400

    Login of user

--- src/main/java/com/tracker/expensestrackerapi/repositories/UserRepositoryImpl.java
+++ src/main/java/com/tracker/expensestrackerapi/repositories/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.tracker.expensestrackerapi.repositories;
 import com.tracker.expensestrackerapi.domain.User;
 import com.tracker.expensestrackerapi.exceptions.EtAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -20,8 +21,12 @@ public class UserRepositoryImpl implements UserRepository{
 
     private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
 
-    private static final String SQL_FIND_BY_ID = "SELECT USER_ID , FIRST_NAME, LAST_NAME, EMAIL, " +
+    private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, " +
             "PASSWORD FROM ET_USERS WHERE USER_ID = ?";
+
+    private static final String SQL_FIND_BY_EMAIL = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, " +
+            "PASSWORD FROM ET_USERS WHERE EMAIL = ?";
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
@@ -45,7 +50,14 @@ public class UserRepositoryImpl implements UserRepository{
 
     @Override
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
-        return null;
+        try {
+            User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
+            if (!password.equals(user.getPassword()))
+                throw new EtAuthException("Invalid email/password");
+            return user;
+        } catch (EmptyResultDataAccessException e) {
+            throw new EtAuthException("Invalid email/password");
+        }
     }
 
     @Override
@@ -59,8 +71,7 @@ public class UserRepositoryImpl implements UserRepository{
     }
 
     private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
-       return new User(
-               rs.getInt("USER_ID"),
+       return new User(rs.getInt("USER_ID"),
                rs.getString("FIRST_NAME"),
                rs.getString("LAST_NAME"),
                rs.getString("EMAIL"),
--- src/main/java/com/tracker/expensestrackerapi/services/UserServicesImpl.java
+++ src/main/java/com/tracker/expensestrackerapi/services/UserServicesImpl.java
@@ -18,7 +18,8 @@ public class UserServicesImpl implements UserService{
 
     @Override
     public User validateUser(String email, String password) throws EtAuthException {
-        return null;
+        if(email != null) email = email.toLowerCase();
+        return userRepository.findByEmailAndPassword(email, password);
     }
 
     @Override
