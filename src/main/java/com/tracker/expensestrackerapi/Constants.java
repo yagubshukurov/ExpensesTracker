373eab7d78f5aafb0a68937600e9659626f404f6
Yagub Shukurov <shukurov23@outlook.com>
2020-11-03 14:19:05 +0400

    Restrict access to path

---
+++ src/main/java/com/tracker/expensestrackerapi/Constants.java
@@ -0,0 +1,8 @@
+package com.tracker.expensestrackerapi;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "expensestrackerapikey";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
--- src/main/java/com/tracker/expensestrackerapi/ExpensesTrackerApiApplication.java
+++ src/main/java/com/tracker/expensestrackerapi/ExpensesTrackerApiApplication.java
@@ -1,7 +1,10 @@
 package com.tracker.expensestrackerapi;
 
+import com.tracker.expensestrackerapi.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpensesTrackerApiApplication {
@@ -10,4 +13,12 @@ public class ExpensesTrackerApiApplication {
 		SpringApplication.run(ExpensesTrackerApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 }
